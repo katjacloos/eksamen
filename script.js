297e1fe3b80b12ee6db1634bf8dbb3dab73915a2
Katja Cloos <katjacloos@hotmail.dk>
2024-12-20 05:10:39 +0100

    final

--- script.js
+++ script.js
@@ -2,22 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
     // Vælg menu-knap og mobilmenu
     const menuBtn = document.querySelector('.menu-btn');
     const mobileMenu = document.getElementById('mobile-menu');
-
     // Tilføj klik-event for at vise/skjule menuen
     menuBtn.addEventListener('click', function () {
         mobileMenu.classList.toggle('show');
     });
 });
-
-
 // Fold-ud funktion til alle nyheder
 document.addEventListener("DOMContentLoaded", () => {
     const titles = document.querySelectorAll(".title");
-
     titles.forEach(title => {
         title.addEventListener("click", () => {
             const extraContent = title.nextElementSibling;
-
             // Toggler display mellem "none" og "block"
             if (extraContent.style.display === "block") {
                 extraContent.style.display = "none";
